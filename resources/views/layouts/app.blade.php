ee1257ca0488c656912218a4f1ac552ae50d4fa6
Micah <micah@asuh.com>
2018-09-24 23:21:44 -0700

    Add skip to main link for a11y
    For better accessibility, added .skip-main link for keyboard users to go straight to the content at #main

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,11 +2,12 @@
 <html {!! get_language_attributes() !!}>
   @include('partials.head')
   <body @php body_class() @endphp>
+    <a class="skip-main" href="#main">{{ __('Skip to content', 'sage') }}</a>
     @php do_action('get_header') @endphp
     @include('partials.header')
     <div class="wrap container" role="document">
       <div class="content">
-        <main class="main">
+        <main class="main" id="main">
           @yield('content')
         </main>
         @if (App\display_sidebar())
