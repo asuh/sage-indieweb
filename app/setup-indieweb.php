6a719c12a3fc211bdc944e53089b1997114e984e
Micah <micah@asuh.com>
2018-12-08 16:42:47 -0800

    Syndication Links and Simple Location filtering
    Check for filters before applying remove_filter() to support the above services

--- app/setup-indieweb.php
+++ app/setup-indieweb.php
@@ -4,16 +4,13 @@
  * Code that improves theme support for various plugins
  */
 add_action('init', function() {
-	/*
- 	* Adds support for Syndication Links
- 	*/
-	if ( class_exists( 'Syn_Meta' ) ) {
-		remove_filter( 'the_content', array( 'Syn_Config', 'the_content' ), 30 );
-	}
-	/*
-	 * Adds support for Simple Location
-	 */
-	if ( class_exists( 'Loc_View' ) ) {
-		remove_filter( 'the_content', array( 'Loc_View', 'location_content' ), 12 );
-	}
+    if ( class_exists( 'Syn_Meta' ) && has_filter( 'the_content', array( 'Syn_Config', 'the_content' ) ) ) {
+        remove_filter( 'the_content', array( 'Syn_Config', 'the_content' ), 30 );
+    }
+    /*
+     * Adds support for Simple Location
+     */
+    if ( class_exists( 'Loc_View' ) && has_filter( 'the_content', array( 'Loc_View', 'location_content' ) ) ) {
+        remove_filter( 'the_content', array( 'Loc_View', 'location_content' ), 12 );
+    }
 }, 11 );
